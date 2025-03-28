e14a6649aec9599640521ab563012626cdc1e6a0
ssj2-z <ssj2@usf.edu>
2025-03-28 12:32:06 -0400

    Task 2: "Sales rep’s clients and total spent for a specific client."

--- cc_17.js
+++ cc_17.js
@@ -1,6 +1,6 @@
 // Task 1 Customer class
 class Customer {
-    constructor(name, email) {
+    const(name, email) {
       this.name = name;
       this.email = email;
       this.purchaseHistory = [];
@@ -14,4 +14,22 @@ class Customer {
       return this.purchaseHistory.reduce((total, amount) => total + amount, 0);
     }
   }
-  
+
+  // Task 2 Sales Class
+  class SalesRep {
+    const(name) {
+        this.name = name;
+        this.clients = [];
+      }
+    
+      // Adding customers to the clients portal list
+      addClient(customer) { 
+        this.clients.push(customer);
+      }
+    
+      // Finding clients by name and or total spent
+      getClientTotal(name) {
+        const client = this.clients.find(client => client.name === name);
+        return client ? client.getTotalSpent() : 0;
+      }
+    }
